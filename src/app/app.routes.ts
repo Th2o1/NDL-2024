cba97bcb292cbb917010366bffb1cf790d16e52c
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2024-12-05 22:34:01 +0100

    fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CreditsComponent } from './components/credits/credits.component';
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'credits', component: CreditsComponent }
+];
