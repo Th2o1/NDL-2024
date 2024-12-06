2bca9bd6eefd6f570479a12079c62b0e46a074dd
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2024-12-06 04:09:29 +0100

    home page desktop

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "@components/nav-bar/nav-bar.component";
 import { CookiesPopupComponent } from "../cookies-popup/cookies-popup.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-homepage',
-  imports: [NavBarComponent, CookiesPopupComponent],
+  imports: [NavBarComponent, CookiesPopupComponent, CommonModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent {
-
+  scales = [1, 2, 3, 4, 5];
 }
