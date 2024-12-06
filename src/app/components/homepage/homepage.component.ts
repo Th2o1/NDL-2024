ad34e2c0cb9d69575a6836c30e5d7531aec71766
Théo Ischia <theoischia@gmail.com>
2024-12-06 03:01:22 +0100

    Modal window added

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NavBarComponent } from "@components/nav-bar/nav-bar.component";
 import { CookiesPopupComponent } from "../cookies-popup/cookies-popup.component";
 
@@ -9,5 +10,16 @@ import { CookiesPopupComponent } from "../cookies-popup/cookies-popup.component"
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent {
+  test(): void {
+    const myDiv = document.getElementById('modular-window');
+    if(myDiv){ // Check if div is here
+      const isHidden = myDiv.style.display === 'none'; // Vérifie si elle est cachée
+      myDiv.style.display = isHidden ? 'block' : 'none'
+    }
+    else{
+      console.log("Error in homepage.components.ts : modular-window not found");
+    }
+    console.log("test")
+  }
 
 }
