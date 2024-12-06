b11182b08c9fa6f53aa2aae1bb5cc519a0b4b454
Felix Harris Ndiaye <felixharrisndiaye@gmail.com>
2024-12-06 03:48:14 +0100

    basic pufferfish for credits

--- src/app/components/credits/contributor/contributor.component.ts
+++ src/app/components/credits/contributor/contributor.component.ts
@@ -1,15 +1,48 @@
-import { Component } from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
+import { Component, OnInit } from '@angular/core';
+import {NgForOf, NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-contributor',
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgForOf
   ],
   templateUrl: './contributor.component.html',
   standalone: true,
   styleUrl: './contributor.component.scss'
 })
-export class ContributorComponent {
+export class ContributorComponent implements OnInit {
+  people: string[] = [];
+  colors: string[] = ['albinos', 'red', 'green', 'blue', 'purple', 'black', 'brown', 'fade'];
+  hoverStates: boolean[] = [];
 
+  ngOnInit() {
+    this.people = [
+      'Pierre',
+      'Théo',
+      'Jaenai',
+      'Lucian',
+      'Constantin',
+      'Audric',
+      'Felix',
+      'Julien'
+    ];
+    this.hoverStates = Array(this.people.length).fill(false);
+  }
+
+  setHoverState(index: number, isHovering: boolean): void {
+    this.hoverStates[index] = isHovering;
+  }
+
+  getPufferfishImage(index: number): string {
+    const color = this.colors[index % this.colors.length];
+    const basePath = "../../../../assets/credits/";
+    const hoverImage = `${basePath}pufferfish_${color}_4_64x64.png`;
+    const defaultImage = `${basePath}pufferfish_${color}_1_64x64.png`;
+
+    if (this.hoverStates[index]) {
+      return `url("` + hoverImage + `") center`;
+    }
+    return `url("` + defaultImage + `") center`;
+  }
 }
